63d6a922397c49a65198b7aa7774f8e3fe7a7e00
Ahmed Ibrahim Metawee' Youssef <64774266+aimtyaem@users.noreply.github.com>
2025-03-14 16:27:58 +0200

    Update git.sh

--- git.sh
+++ git.sh
@@ -9,6 +9,6 @@ git commit -m "Initial commit"
 
 # Create new repository on GitHub (via website)
 # Then link and push
-git remote add origin https://github.com/your-username/your-repo.git
+git remote add origin https://github.com/aimtyaem/EOAgriTool
 git branch -M main
 git push -u origin main
