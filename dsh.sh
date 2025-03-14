7313c39baa1564985e928e8dda7d43464bc4c4bb
Ahmed Ibrahim Metawee' Youssef <64774266+aimtyaem@users.noreply.github.com>
2025-03-14 18:38:16 +0200

    Update dsh.sh

--- dsh.sh
+++ dsh.sh
@@ -1,3 +1,6 @@
+git add dsh.sh
+git commit -m "Make dsh.sh executable"
 git checkout 6762140e4c683be2a960c72915e5ad5c96d893e8 -- dashboard.html
+chmod +x dsh.sh
 git commit -m "Revert dashboard.html to previous version before updates"
 git push origin main
