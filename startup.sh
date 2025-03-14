52529959faacdd58ae4aee874775179cb0b9ff3a
Ahmed Ibrahim Metawee' Youssef <64774266+aimtyaem@users.noreply.github.com>
2025-03-14 20:01:03 +0200

    Update startup.sh
    Update

--- startup.sh
+++ startup.sh
@@ -1,1 +1,9 @@
 gunicorn --bind=0.0.0.0:$PORT --timeout 600 app:app
+Name: WEBSITES_PORT
+   Value: 8000
+
+   Name: SCM_DO_BUILD_DURING_DEPLOYMENT
+   Value: true
+
+   Name: WEBSITE_WEBDEPLOY_USE_SCM
+   Value: true
