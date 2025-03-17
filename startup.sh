1cc9aceb40405ebaa9acfcca4659832246d3056e
Ahmed Ibrahim Metawee' Youssef <64774266+aimtyaem@users.noreply.github.com>
2025-03-17 14:29:11 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,3 +4,6 @@ git commit -m "Initial commit"
 git branch -M main
 git remote add origin https://github.com/aimtyaem/EOAgriTool.git
 git push -u origin main
+git add .
+git commit -m "Move dashboard to GitHub Pages"
+git push origin main
